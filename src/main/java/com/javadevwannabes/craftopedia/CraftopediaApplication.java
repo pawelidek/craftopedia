1370ded18fc64f326627b599fe5b8977186dcae6
Paweł Idek <pawelidek@op.pl>
2019-11-05 15:47:35 +0100

    Initial commit

---
+++ src/main/java/com/javadevwannabes/craftopedia/CraftopediaApplication.java
@@ -0,0 +1,13 @@
+package com.javadevwannabes.craftopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftopediaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CraftopediaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/javadevwannabes/craftopedia/CraftopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadevwannabes.craftopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftopediaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
